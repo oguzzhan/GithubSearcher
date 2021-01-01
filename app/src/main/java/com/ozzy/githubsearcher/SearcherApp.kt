870eeb8e4df5fa04ab17fe8dad94bc4f9eee3b64
Oğuzhan <oguzhan.karacan@loodos.com>
2021-01-01 15:58:58 +0300

    app class implemented

---
+++ app/src/main/java/com/ozzy/githubsearcher/SearcherApp.kt
@@ -0,0 +1,10 @@
+package com.ozzy.githubsearcher
+
+import androidx.multidex.MultiDexApplication
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Created by Oğuzhan Karacan on 1.01.2021.
+ */
+@HiltAndroidApp
+class SearcherApp : MultiDexApplication()
